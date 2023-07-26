fb0754633389e30a5eb30c9e94551b82f78cc41b
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-07-26 08:32:52 +0300

    fix

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(express.json());
 // app.use("/api/users", expenseRouter);
 // app.use("/api/users", incomeRouter);
 
+// Why it is no works?
+
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
